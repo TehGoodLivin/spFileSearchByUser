23e7594732bb9f7940fbe5349197555b5c6bfdd7
Austin L <53409880+TheRealGoodLivin@users.noreply.github.com>
2022-03-29 11:09:54 +0200

    Update spFileSearchByUser.ps1

--- spFileSearchByUser.ps1
+++ spFileSearchByUser.ps1
@@ -149,5 +149,5 @@ if ($parentSiteOnly -eq $false) {
     }
 }
 
-Write-Host "$(get-date -format yyyy/MM/dd-HH:mm:ss) - Script Complete" -ForegroundColor Green
 Disconnect-PnPOnline
+Write-Host "$(get-date -format yyyy/MM/dd-HH:mm:ss) - Script Complete" -ForegroundColor Green
